5c8932b05268ab085608b366d4201147c3d8cdb1
Fabiano Brito <fabiano99.fbs@gmail.com>
2025-03-07 15:13:00 -0300

    api

--- config/mysql.config.js
+++ config/mysql.config.js
@@ -1,21 +1,25 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
+  waitForConnections: true,
+  connectionLimit: 10, // Número de conexões simultâneas
+  queueLimit: 0,
   ssl: { rejectUnauthorized: true } // Necessário para conexões seguras
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error("❌ Erro ao conectar ao banco:", err);
     return;
   }
   console.log("✅ Conectado ao banco de dados Railway!");
+  connection.release();
 });
 
-module.exports = connection;
+module.exports = pool.promise(); // Usa `promise()` para trabalhar com async/await
